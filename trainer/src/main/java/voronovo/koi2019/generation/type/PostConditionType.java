4047c47bfb220312cddbe64097414e1911fe5dcc
Cheslav <free_gallos@mail.ru>
2019-10-10 21:07:22 +0300

    PostConditionType не влез в коммит

--- trainer/src/main/java/voronovo/koi2019/generation/type/PostConditionType.java
+++ trainer/src/main/java/voronovo/koi2019/generation/type/PostConditionType.java
@@ -1,10 +1,10 @@
-package voronovo.koi2019.generation.condition;
+package voronovo.koi2019.generation.type;
 
+import voronovo.koi2019.condition.PostCondition;
 import voronovo.koi2019.entity.Test;
 import voronovo.koi2019.generation.test.DefaultTestBuilder;
 
-import java.util.EnumSet;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static voronovo.koi2019.generation.util.ConstantsHolder.*;
@@ -63,6 +63,33 @@ public enum PostConditionType {
         public PostCondition getCondition(String value) {
             return new PostCondition(this, value);
         }
+    },
+    NOT_EQUAL("not equal") {
+        @Override
+        public boolean isInvalid(Test test) {
+            return test.getCorrectAnswer().equals(test.getExpression());
+        }
+
+        @Override
+        public void modify(Test test, PostCondition condition, DefaultTestBuilder builder) {
+            Map<String, Integer> variablesMap = builder.getVariablesMap();
+            Set<Integer> uniqueSet = new HashSet<>();
+            boolean isUnique = variablesMap //берём мапу переменных
+                    .keySet() //названия переменных
+                    .stream()
+                    .filter(variable -> condition.getValue().contains(variable)) //оставляем те названия, что указаны в настройке
+                    .map(variablesMap::get) //берём их числовые значения
+                    .allMatch(uniqueSet::add); //добавляем в Set, если не добавило - значит есть одинаковые числа
+            if(!isUnique) { //в isInvalid(Test test) откидываем этот тест, делая такую замену и проверку
+                test.setExpression("invalid");
+                test.setCorrectAnswer("invalid");
+            }
+        }
+
+        @Override
+        public PostCondition getCondition(String value) {
+            return new PostCondition(this, value);
+        }
     };
 
     private final String identifier;
@@ -73,7 +100,7 @@ public enum PostConditionType {
 
     public static PostCondition find(String condition) {
         return EnumSet.allOf(PostConditionType.class).stream().filter(value -> condition.contains(value.identifier)).findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("invalid condition identifier for " + condition))
+                .orElseThrow(() -> new IllegalArgumentException("invalid post condition identifier for " + condition))
                 .getCondition(condition.trim());
     }
 
