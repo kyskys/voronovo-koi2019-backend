b134e2e9001110a51ebc599e2bf5740fbc40d647
zemba <chaslau_zemba@senla.eu>
2019-10-15 18:28:24 +0300

    инициализация firebase

---
+++ trainer/src/main/java/voronovo/koi2019/firebase/FirebaseService.java
@@ -0,0 +1,49 @@
+package voronovo.koi2019.firebase;
+
+import com.google.firebase.messaging.*;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.concurrent.ExecutionException;
+
+@Service
+public class FirebaseService {
+//
+//    public void sendMessageWithoutData(PushNotificationRequest request)
+//            throws InterruptedException, ExecutionException {
+//        Message message = getPreconfiguredMessageWithoutData(request);
+//        String response = sendAndGetResponse(message);
+//        logger.info("Sent message without data. Topic: " + request.getTopic() + ", " + response);
+//    }
+//
+//    private String sendAndGetResponse(Message message) throws InterruptedException, ExecutionException {
+//        return FirebaseMessaging.getInstance().sendAsync(message).get();
+//    }
+//
+//    private AndroidConfig getAndroidConfig(String topic) {
+//        return AndroidConfig.builder()
+//                .setTtl(Duration.ofMinutes(2).toMillis()).setCollapseKey(topic)
+//                .setPriority(AndroidConfig.Priority.HIGH)
+//                .setNotification(AndroidNotification.builder().setSound(NotificationParameter.SOUND.getValue())
+//                        .setColor(NotificationParameter.COLOR.getValue()).setTag(topic).build()).build();
+//    }
+//
+//    private ApnsConfig getApnsConfig(String topic) {
+//        return ApnsConfig.builder()
+//                .setAps(Aps.builder().setCategory(topic).setThreadId(topic).build()).build();
+//    }
+//
+//    private Message getPreconfiguredMessageWithoutData(PushNotificationRequest request) {
+//        return getPreconfiguredMessageBuilder(request).setTopic(request.getTopic())
+//                .build();
+//    }
+//
+//    private Message.Builder getPreconfiguredMessageBuilder(PushNotificationRequest request) {
+//        AndroidConfig androidConfig = getAndroidConfig(request.getTopic());
+//        ApnsConfig apnsConfig = getApnsConfig(request.getTopic());
+//        return Message.builder()
+//                .setApnsConfig(apnsConfig).setAndroidConfig(androidConfig).setNotification(
+//                        new Notification(request.getTitle(), request.getMessage()));
+//    }
+
+}
