c8e69974dafcd775d0e67fd093f411781dea3476
kyskys <free_gallos@mail.ru>
2019-10-19 18:04:18 +0300

    фикс времени в запросе поиска последней задачи дня

--- trainer/src/main/java/voronovo/koi2019/repository/QuestionRepository.java
+++ trainer/src/main/java/voronovo/koi2019/repository/QuestionRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.RequestParam;
 import voronovo.koi2019.entity.Question;
 
@@ -11,7 +12,9 @@ import java.util.Date;
 
 @RepositoryRestResource
 public interface QuestionRepository extends PagingAndSortingRepository<Question, Long> {
-    Question findFirstByCreatedAtBeforeOrderByCreatedAtDesc(@RequestParam Date beforeDate);
+    Question findFirstByCreatedAtBeforeOrderByCreatedAtDesc(@RequestParam
+                                                            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+                                                                    Date beforeDate);
 
     @Modifying
     @Query("update Question q set q.winner=:winner where id=:id")
