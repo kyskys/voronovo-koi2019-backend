5fa724f7a5abcf3731a9a76639f39f2f476b6411
zemba <chaslau_zemba@senla.eu>
2019-10-23 16:40:25 +0300

    добавлена версионность

---
+++ trainer/src/main/java/voronovo/koi2019/controller/VersionController.java
@@ -0,0 +1,27 @@
+package voronovo.koi2019.controller;
+
+import com.sun.javafx.binding.StringFormatter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VersionController {
+
+    @Value("#{${app.version}}")
+    private String version;
+
+    @Value("#{${app.version.href}}")
+    private String href;
+
+    @GetMapping("version")
+    public ResponseEntity<String> getVersion() {
+        return ResponseEntity.ok(
+                String.format("{" +
+                        "\"version\": %s, " +
+                        "\"href\": %s" +
+                        "}", version, href)
+        );
+    }
+}
